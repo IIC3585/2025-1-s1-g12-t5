6d35da45cb228996febd50dc6b4d98557c3c71c1
Matías Adauy <adauym@gmail.com>
2025-06-22 22:16:04 -0400

    added combobox in react with shadcn/ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,28 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
+
+import svelte from '@astrojs/svelte';
+
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+
+  integrations: [
+		mdx(), 
+		sitemap(), 
+		react({
+      include: ['**/react/*']
+  	}), 
+		svelte({
+      include: ['**/svelte/*']
+  	}),
+	],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
